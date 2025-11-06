92c45a47bdae0f14c0d27af2df86f0b7990ebe28
Kirill Sirotkin <kirill.sirotkin.dev@gmail.com>
2025-11-06 21:44:15 +0200

    feat: make bulkpool generic

--- bulkpool/bulkpool.go
+++ bulkpool/bulkpool.go
@@ -1,37 +1,41 @@
 package bulkpool
 
-type BulkPool struct {
+import "sync"
+
+type BulkPool[T any, R any] struct {
 	workers int
-	jobs    []int
-	process func(int) int
+	jobs    []T
+	process func(T) R
 	wg      sync.WaitGroup
 }
 
-func New(workers int, jobs []int, process func(int) int) *BulkPool {
-	return &BulkPool{
+func New[T any, R any](workers int, jobs []T, process func(T) R) *BulkPool[T, R] {
+	return &BulkPool[T, R]{
 		workers: workers,
 		jobs:    jobs,
 		process: process,
 	}
 }
 
-func (bp *BulkPool) Run() []int {
-	results := make([]int, len(bp.jobs))
+func (bp *BulkPool[T, R]) Run() []R {
+	results := make([]R, len(bp.jobs))
 	jobChan := make(chan int, len(bp.jobs))
+
 	for i := 0; i < bp.workers; i++ {
 		bp.wg.Add(1)
 		go func() {
 			defer bp.wg.Done()
-			for job := range jobChan {
-				results[job] = bp.process(job)
+			for idx := range jobChan {
+				results[idx] = bp.process(bp.jobs[idx])
 			}
 		}()
 	}
 
 	for i := 0; i < len(bp.jobs); i++ {
-		jobChan <- bp.jobs[i]
+		jobChan <- i
 	}
 	close(jobChan)
+
 	bp.wg.Wait()
 	return results
 }
