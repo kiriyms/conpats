1915c495552a18c599f19b19e851ccf5e526b3d6
Kirill Sirotkin <kirill.sirotkin.dev@gmail.com>
2025-11-06 21:08:28 +0200

    feat: add simple worker pool

--- workerpool.go
+++ workerpool.go
@@ -5,7 +5,25 @@ import (
 	"sync"
 )
 
-func Worker(id int, in <-chan int, out chan<- int, f func(int) int, wg *sync.WaitGroup) {
+func WorkerPool(numWorkers int, jobs []int, f func(int) int) <-chan int {
+	in := make(chan int, len(jobs))
+	out := make(chan int, len(jobs))
+	var wg sync.WaitGroup
+
+	for w := 1; w <= numWorkers; w++ {
+		wg.Add(1)
+		go worker(w, in, out, f, &wg)
+	}
+
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+
+	return out
+}
+
+func worker(id int, in <-chan int, out chan<- int, f func(int) int, wg *sync.WaitGroup) {
 	defer wg.Done()
 	for j := range in {
 		fmt.Printf("Worker %d processing job %d\n", id, j)
