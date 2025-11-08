8ca63e54f79428360317eccd760efa2a4ff7e6db
Kirill Sirotkin <kirill.sirotkin.dev@gmail.com>
2025-11-08 15:29:14 +0200

    feat: add basic test, add reusability test

--- pool/pool_test.go
+++ pool/pool_test.go
@@ -1,8 +1,7 @@
 package pool_test
 
 import (
-	"fmt"
-	"runtime"
+	"sync/atomic"
 	"testing"
 	"time"
 
@@ -10,28 +9,64 @@ import (
 )
 
 func TestPool(t *testing.T) {
+	t.Run("basic", func(t *testing.T) {
+		p := pool.New(7)
+		jobCount := 50
+		var completed atomic.Int64
 
-	fmt.Println(runtime.NumGoroutine())
+		for i := 0; i < jobCount; i++ {
+			p.Add(func() {
+				time.Sleep(2 * time.Millisecond)
+				completed.Add(1)
+			})
+		}
 
-	p := pool.New(5)
+		p.CloseAndWait()
+		if completed.Load() != int64(jobCount) {
+			t.Errorf("Jobs expected: %d, got: %d", jobCount, completed.Load())
+		}
+	})
 
-	fmt.Println(runtime.NumGoroutine())
+	t.Run("is reuseable", func(t *testing.T) {
+		p := pool.New(7)
+		jobCount := 50
+		var completedTotal atomic.Int64
 
-	for i := range 10 {
-		p.Add(func() {
-			time.Sleep(2 * time.Second)
-			fmt.Printf("job-%d\n", i)
-		})
-	}
+		var completedPartA atomic.Int64
+		for i := 0; i < jobCount; i++ {
+			p.Add(func() {
+				time.Sleep(2 * time.Millisecond)
+				completedTotal.Add(1)
+				completedPartA.Add(1)
+			})
+		}
 
-	p.Wait()
+		p.Wait()
+		if completedPartA.Load() != int64(jobCount) {
+			t.Errorf("Part A jobs expected: %d, got: %d", jobCount, completedPartA.Load())
+		}
+		if completedPartA.Load() != completedTotal.Load() {
+			t.Errorf("Expected Part A equal to total. Part A: %d, Total: %d", completedPartA.Load(), completedTotal.Load())
+		}
 
-	for i := range 10 {
-		p.Add(func() {
-			time.Sleep(2 * time.Second)
-			fmt.Printf("job-%d\n", i)
-		})
-	}
+		var completedPartB atomic.Int64
+		for i := 0; i < jobCount; i++ {
+			p.Add(func() {
+				time.Sleep(2 * time.Millisecond)
+				completedTotal.Add(1)
+				completedPartB.Add(1)
+			})
+		}
 
-	p.CloseAndWait()
+		p.CloseAndWait()
+		if completedPartB.Load() != int64(jobCount) {
+			t.Errorf("Part B jobs expected: %d, got: %d", jobCount, completedPartB.Load())
+		}
+		if completedPartB.Load() != completedPartA.Load() {
+			t.Errorf("Expected Part A equal to Part B. Part A: %d, Part B: %d", completedPartA.Load(), completedPartB.Load())
+		}
+		if completedPartB.Load()+completedPartA.Load() != completedTotal.Load() {
+			t.Errorf("Expected sum of Part A and Part B to equal to total. Parts A + B: %d, Total: %d", completedPartA.Load()+completedPartB.Load(), completedTotal.Load())
+		}
+	})
 }
