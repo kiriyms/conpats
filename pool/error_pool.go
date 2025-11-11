be2d8a597166f20972de9e9ff1c2c7df0925b0ec
Kirill Sirotkin <kirill.sirotkin.dev@gmail.com>
2025-11-11 20:07:01 +0200

    feat: implement Error Pool

---
+++ pool/error_pool.go
@@ -0,0 +1,51 @@
+package pool
+
+import (
+	"errors"
+	"sync"
+)
+
+type ErrorJob func() error
+
+type ErrorPool struct {
+	pool Pool
+
+	mu   sync.Mutex
+	errs []error
+}
+
+func (p *ErrorPool) Go(job ErrorJob) bool {
+	return p.pool.Go(func() {
+		p.addErr(job())
+	})
+}
+
+func (p *ErrorPool) Wait() error {
+	p.pool.Wait()
+	return p.getErrs()
+}
+
+func (p *ErrorPool) CloseAndWait() error {
+	p.pool.CloseAndWait()
+	return p.getErrs()
+}
+
+func (p *ErrorPool) getErrs() error {
+	p.mu.Lock()
+	errs := p.errs
+	p.errs = nil
+	p.mu.Unlock()
+
+	if len(errs) == 0 {
+		return nil
+	}
+	return errors.Join(errs...)
+}
+
+func (p *ErrorPool) addErr(err error) {
+	if err != nil {
+		p.mu.Lock()
+		p.errs = append(p.errs, err)
+		p.mu.Unlock()
+	}
+}
