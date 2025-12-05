4df3f6926f1a7cea96a8bdd9ec76508f048d8795
Kirill Sirotkin <kirill.sirotkin.dev@gmail.com>
2025-12-05 19:01:59 +0200

    feat: add multiple-typed Pipeline base

--- pipeline/pipeline.go
+++ pipeline/pipeline.go
@@ -1,1 +1,33 @@
 package pipeline
+
+type stageFunc[I any, O any] func(I) O
+
+type stage[I any, O any] struct {
+	fn      stageFunc[I, O]
+	workers int
+}
+
+type Pipeline[I any] struct {
+	stages []any
+}
+
+func New[I any]() *Pipeline[I] {
+	return &Pipeline[I]{
+		stages: make([]any, 0),
+	}
+}
+
+func AddStage[I, O any](p *Pipeline[I], fn func(I) O, workers int) *Pipeline[O] {
+	if workers <= 0 {
+		workers = 1
+	}
+
+	newStages := append(make([]any, 0), p.stages...)
+	newStages = append(newStages, stage[I, O]{fn: fn, workers: workers})
+
+	return &Pipeline[O]{
+		stages: newStages,
+	}
+}
+
+
