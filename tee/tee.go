1789ac9469a15418ee0d45ec390a554277837e9f
Kirill Sirotkin <kirill.sirotkin.dev@gmail.com>
2025-12-15 18:11:50 +0200

    feat: rename Tee cinstructor, add buffered channel option

--- tee/tee.go
+++ tee/tee.go
@@ -1,14 +1,17 @@
 package tee
 
-func Tee[I any](in <-chan I, n int) []<-chan I {
+func NewTee[I any](in <-chan I, n int, buf int) []chan I {
 	if n <= 0 {
 		n = 1
 	}
 
 	outs := make([]chan I, n)
 	for i := range n {
-		out := make(chan I)
-		outs[i] = out
+		if buf <= 0 {
+			outs[i] = make(chan I)
+		} else {
+			outs[i] = make(chan I, buf)
+		}
 	}
 
 	go func() {
@@ -25,9 +28,5 @@ func Tee[I any](in <-chan I, n int) []<-chan I {
 		}
 	}()
 
-	result := make([]<-chan I, n)
-	for i, out := range outs {
-		result[i] = out
-	}
-	return result
+	return outs
 }
