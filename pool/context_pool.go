5eef4cfefda1267a16d4cce647aa7478a2e2cd40
Kirill Sirotkin <kirill.sirotkin.dev@gmail.com>
2025-12-19 20:22:53 +0200

    feat: add Context Pool cancelOnErr

--- pool/context_pool.go
+++ pool/context_pool.go
@@ -7,19 +7,33 @@ import (
 type ContextPool struct {
 	errorPool *ErrorPool
 
+	cancelOnErr bool
+
 	ctx    context.Context
 	cancel context.CancelFunc
 }
 
 func (p *ContextPool) Go(job func(context.Context) error) {
 	p.errorPool.Go(func() error {
-		return job(p.ctx)
+		err := job(p.ctx)
+		if err != nil && p.cancelOnErr {
+			p.cancel()
+			p.errorPool.addErr(err)
+		}
+
+		return err
 	})
 }
 
 func (p *ContextPool) TryGo(job func(context.Context) error) bool {
 	return p.errorPool.TryGo(func() error {
-		return job(p.ctx)
+		err := job(p.ctx)
+		if err != nil && p.cancelOnErr {
+			p.cancel()
+			p.errorPool.addErr(err)
+		}
+
+		return err
 	})
 }
 
@@ -29,10 +43,15 @@ func (p *ContextPool) Collect() []error {
 
 func (p *ContextPool) Wait() []error {
 	if p.cancel != nil {
-		p.cancel()
+		defer p.cancel()
 	}
 
 	err := p.errorPool.Wait()
 
 	return err
+}
+
+func (p *ContextPool) WithCancelOnError(cancelOnErr bool) *ContextPool {
+	p.cancelOnErr = cancelOnErr
+	return p
 }
