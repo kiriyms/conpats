d88a75613c5b9aee8acd7d7d9fb3d6d1d7bb554d
Kirill Sirotkin <kirill.sirotkin.dev@gmail.com>
2025-12-21 18:19:49 +0200

    feat: add more Context Pool tests for coverage

--- pool/context_pool_test.go
+++ pool/context_pool_test.go
@@ -142,4 +142,199 @@ func TestContextPool(t *testing.T) {
 			t.Errorf("Expected 4 cancelled jobs, got: %d", cancelled.Load())
 		}
 	})
+
+	t.Run("handles TryGo correctly", func(t *testing.T) {
+		t.Parallel()
+
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		p := pool.New(1).WithErrors(false).WithContext(ctx)
+		ok := p.TryGo(func(c context.Context) error {
+			time.Sleep(2 * time.Millisecond)
+			return nil
+		})
+
+		if !ok {
+			t.Fatalf("Should not error on .TryGo() if the Pool is not closed yet")
+		}
+
+		p.Wait()
+
+		ok = p.TryGo(func(c context.Context) error {
+			time.Sleep(2 * time.Millisecond)
+			return nil
+		})
+
+		if ok {
+			t.Fatalf("Expected error on .TryGo() because Pool is closed")
+		}
+	})
+
+	t.Run("cancels on error correctly", func(t *testing.T) {
+		t.Parallel()
+
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		p := pool.New(5).WithErrors(false).WithContext(ctx).WithCancelOnError(true)
+		jobCount := 50
+		var completed atomic.Int64
+		var cancelled atomic.Int64
+
+		for i := 0; i < jobCount; i++ {
+			p.Go(func(c context.Context) error {
+				select {
+				case <-c.Done():
+					cancelled.Add(1)
+					return c.Err()
+				case <-time.After(2 * time.Millisecond):
+					if completed.Load() == 5 {
+						cancelled.Add(1)
+						return fmt.Errorf("intentional error")
+					}
+					completed.Add(1)
+					return nil
+				}
+			})
+		}
+
+		errs := p.Wait()
+		if len(errs) != 45 {
+			t.Errorf("Expected 45 errors, got: %d", len(errs))
+		}
+		if completed.Load() != 5 {
+			t.Errorf("Expected 5 completed jobs, got: %d", completed.Load())
+		}
+		if cancelled.Load() != 45 {
+			t.Errorf("Expected 45 cancelled jobs, got: %d", cancelled.Load())
+		}
+	})
+
+	t.Run("cancels correctly on error and returns only first error", func(t *testing.T) {
+		t.Parallel()
+
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		p := pool.New(5).WithErrors(true).WithContext(ctx).WithCancelOnError(true)
+		jobCount := 50
+		var completed atomic.Int64
+		var cancelled atomic.Int64
+
+		for i := 0; i < jobCount; i++ {
+			p.Go(func(c context.Context) error {
+				select {
+				case <-c.Done():
+					cancelled.Add(1)
+					return c.Err()
+				case <-time.After(2 * time.Millisecond):
+					if completed.Load() == 5 {
+						cancelled.Add(1)
+						return fmt.Errorf("intentional error")
+					}
+					completed.Add(1)
+					return nil
+				}
+			})
+		}
+
+		errs := p.Wait()
+		if len(errs) != 1 {
+			t.Errorf("Expected only first error, got: %d", len(errs))
+		}
+		if completed.Load() != 5 {
+			t.Errorf("Expected 5 completed jobs, got: %d", completed.Load())
+		}
+		if cancelled.Load() != 45 {
+			t.Errorf("Expected 45 cancelled jobs, got: %d", cancelled.Load())
+		}
+		if errs[0].Error() != "intentional error" {
+			t.Errorf("Expected 'intentional error', got: %v", errs[0])
+		}
+	})
+
+	t.Run("handles TryGo correctly with cancel on error", func(t *testing.T) {
+		t.Parallel()
+
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		p := pool.New(1).WithErrors(true).WithContext(ctx).WithCancelOnError(true)
+		ok := p.TryGo(func(c context.Context) error {
+			time.Sleep(2 * time.Millisecond)
+			return fmt.Errorf("intentional error")
+		})
+
+		if !ok {
+			t.Fatalf("Should not error on .TryGo() if the Pool is not closed yet")
+		}
+
+		p.Wait()
+
+		ok = p.TryGo(func(c context.Context) error {
+			time.Sleep(2 * time.Millisecond)
+			return nil
+		})
+
+		if ok {
+			t.Fatalf("Expected error on .TryGo() because Pool is closed")
+		}
+	})
+
+	t.Run("collects errors correctly before Wait", func(t *testing.T) {
+		t.Parallel()
+		
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		p := pool.New(3).WithErrors(false).WithContext(ctx)
+		jobCount := 50
+		var completed atomic.Int64
+		var errored atomic.Int64
+
+		for i := 0; i < jobCount; i++ {
+			p.Go(func(c context.Context) error {
+				time.Sleep(2 * time.Millisecond)
+				completed.Add(1)
+
+				if i%5 == 0 {
+					errored.Add(1)
+					return fmt.Errorf("err%d", i)
+				}
+				return nil
+			})
+		}
+
+		collectedErrs := p.Collect()
+		if completed.Load() != int64(jobCount) {
+			t.Errorf("Jobs expected after Collect: %d, got: %d", errored.Load(), completed.Load())
+		}
+		if len(collectedErrs) != int(errored.Load()) {
+			t.Errorf("Errors count mismatch after Collect: count: %d, collected: %d", errored.Load(), len(collectedErrs))
+		}
+
+		errored.Store(0)
+
+		for i := 0; i < jobCount; i++ {
+			p.Go(func(c context.Context) error {
+				time.Sleep(2 * time.Millisecond)
+				completed.Add(1)
+
+				if i%5 == 0 {
+					errored.Add(1)
+					return fmt.Errorf("err%d", i)
+				}
+				return nil
+			})
+		}
+
+		errs := p.Wait()
+		if completed.Load() != int64(jobCount*2) {
+			t.Errorf("Jobs expected after Wait: %d, got: %d", jobCount*2, completed.Load())
+		}
+		if len(errs) != int(errored.Load()) {
+			t.Errorf("Errors count mismatch; count: %d, collected: %d", errored.Load(), len(errs))
+		}
+	})
 }
