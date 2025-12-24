71cbb78db077bb399432a042116a41fc14316c8f
Kirill Sirotkin <kirill.sirotkin.dev@gmail.com>
2025-12-24 22:33:04 +0200

    feat: add Tee annotation, add buffer size validation

--- tee/tee.go
+++ tee/tee.go
@@ -1,8 +1,14 @@
 package tee
 
+// NewTee takes an input channel and returns n output channels that each receive all items from the input channel.
+//
+// A buffer size can be specified for the output channels; if buf is 0 or negative, unbuffered channels are created.
 func NewTee[I any](in <-chan I, n int, buf int) []chan I {
 	if n <= 0 {
 		n = 1
+	}
+	if buf <= 0 {
+		buf = 0
 	}
 
 	outs := make([]chan I, n)
